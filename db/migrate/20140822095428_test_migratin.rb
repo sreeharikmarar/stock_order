301b702d902bd8e2112240e569953ba314280e2d
sreeharikmarar <sreeharikmarar@gmail.com>
2014-08-22 15:43:08 +0530

    first commit

---
+++ db/migrate/20140822095428_test_migratin.rb
@@ -0,0 +1,13 @@
+class TestMigratin < ActiveRecord::Migration
+  def up
+	create_table :users  do |t|
+		t.string :name
+		t.string :email
+	end
+  end
+
+  def down
+	drop_table :users 
+	
+  end
+end
---
+++ main.rb
@@ -0,0 +1,47 @@
+require 'rubygems'
+require 'active_record'
+#require 'mechanize'
+require 'nokogiri'
+require 'active_record'
+require 'mysql2'
+
+
+ActiveRecord::Base.establish_connection ({
+  :adapter => "mysql2",
+  :host => "localhost",
+  :username => "root",
+  :password => "prescience",
+  :database => "test"})
+
+
+
+
+class User < ActiveRecord::Base
+      attr_accessor :name , :email
+    
+
+end
+
+
+class Stock
+ 
+  def initialize
+    
+    @prompt = ["name","email"]
+    @user = User.new
+    
+  end
+  def inputs
+     u = User.new
+     @prompt.each do |s|
+       puts "Enter your : #{s}"
+       @user["#{s}"] = gets.chomp
+     end
+    @user.save 
+  end
+end
+
+s=Stock.new
+s.inputs
+
+
